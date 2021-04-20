e1a897251af5aea253b90ee4cbe916b9eb3a294b
Alejandro Novellino <alnovellino07@gmail.com>
2021-04-20 13:06:22 -0400

    Finished the project

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { element } from "prop-types";
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 //create your first component
 export function Home() {
+	let [lightsList, setlightsList] = useState(["red", "yellow", "green"]);
+	let [selected, setSelected] = useState(null);
+
+	let changeLight = key => {
+		if (selected == key) {
+			setSelected(null);
+		} else {
+			setSelected(key);
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container fluid className="bg-light vw-100 vh-100">
+			<Row>
+				<Col>
+					<div className="post"></div>
+				</Col>
+			</Row>
+			<Row>
+				<Col className="">
+					<div className="d-flex flex-column rounded-lg m-auto p-3 trafficLightBody">
+						{lightsList.map((element, index) => {
+							let clasN =
+								"rounded-circle mx-auto light " + element;
+							if (index == selected) clasN += " selected";
+							return (
+								<div
+									key={index}
+									className={clasN}
+									onClick={() => {
+										changeLight(index);
+									}}></div>
+							);
+						})}
+					</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
